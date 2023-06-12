245eeb9202df2d687390a44b5ecf39aa2f9a946d
Jakub <jakubpal30@gmail.com>
2023-06-12 18:04:52 +0200

    Add design

--- app/src/main/java/com/example/gymguru/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/gymguru/presentation/ui/theme/Type.kt
@@ -2,33 +2,101 @@ package com.example.gymguru.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.gymguru.R
+
+val QuickSand = FontFamily(
+    Font(R.font.quicksand_bold, FontWeight.Bold),
+    Font(R.font.quicksand_light, FontWeight.Light),
+    Font(R.font.quicksand_medium, FontWeight.Medium),
+    Font(R.font.quicksand_regular, FontWeight.Normal),
+    Font(R.font.quicksand_semi_bold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Bold,
+        fontSize = 56.sp,
+        lineHeight = 60.sp,
+        letterSpacing = 4.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 50.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 2.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 46.sp,
+        lineHeight = 50.sp,
+        letterSpacing = 2.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = QuickSand
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = QuickSand
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = QuickSand
+    ),
+    titleLarge = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 26.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 2.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 1.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 1.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = QuickSand,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    bodyMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 12.sp,
+        letterSpacing = 0.2.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = QuickSand
+    ),
+    labelMedium = TextStyle(
+        fontFamily = QuickSand
+    ),
+    labelSmall = TextStyle(
+        fontFamily = QuickSand
     )
-    */
 )
